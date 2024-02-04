4e59d5034fc456f5540bf92d6485e870fdb2b780
Miguel Brito <miguel.brito@levva.io>
2024-02-04 18:32:18 -0300

    feat: adding initial structure for the api

---
+++ Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace books_api.Models
+{
+    public class Book
+    {
+        public string? Title { get; set; }
+        public string? Genre { get; set; }
+        public string? Author { get; set; }
+        public string? Editor { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
